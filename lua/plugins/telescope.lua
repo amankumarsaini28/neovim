2b5052797d5503b472d0454fc96f6578f37913b0
Saini, Aman Kumar <amankumarsaini28@gmail.com>
2025-06-24 23:26:23 +0530

    scoping shit to grep on cwd

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,4 +3,17 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.5",
   dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    defaults = {
+      cwd = vim.loop.cwd(),
+    },
+    pickers = {
+      find_files = {
+        cwd = vim.loop.cwd(),
+      },
+      live_grep = {
+        cwd = vim.loop.cwd(),
+      },
+    },
+  },
 }
