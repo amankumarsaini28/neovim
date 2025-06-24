f0aef52ca4c5b3e03872957c486da09a2e003235
Saini, Aman Kumar <amankumarsaini28@gmail.com>
2025-06-24 22:28:17 +0530

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+--  then
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.api.nvim_set_keymap("n", "<leader>gs", ":Neogit<CR>", { noremap = true, silent = true }) -- Open Neogit status
+vim.api.nvim_set_keymap("n", "<leader>ct", ":Telescope colorscheme<CR>", { noremap = true, silent = true }) -- Open Theme List
---
+++ lua/plugins/ayu.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "Shatur/neovim-ayu",
+    config = function()
+      -- Optional: set the ayu style you prefer
+      vim.g.ayucolor = "mirage" -- Options: mirage, light, dark
+    end,
+  },
+}
